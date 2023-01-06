d7eaf00d61fd4c4fd83d8d34e761ddc3f0c15041
Fulya <fulya.ertay@gmail.com>
2023-01-06 22:21:20 +0300

    feat:adjust image

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="h-full w-full flex items-center justify-center ">
-      <div className='w-[380px] h-[581px] bg-red-300'>
-        <img className="w-[380px] h-[581px]" src="	https://static.cdninstagram.com/rsrc.php/v3/yr/r/fzBXVxs22bH.png"></img>
+      <div className='w-[380px] h-[581px] bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px]'>
+      
       </div>
      
     </div>
