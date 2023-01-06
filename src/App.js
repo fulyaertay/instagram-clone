b08e902bca44cc323034a06ad5767ff004ff24be
Fulya <fulya.ertay@gmail.com>
2023-01-06 22:11:09 +0300

    feat:added login

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="h-full w-full flex items-center justify-center ">
+      <div className='w-[380px] h-[581px] bg-red-300'>
+        <img className="w-[380px] h-[581px]" src="	https://static.cdninstagram.com/rsrc.php/v3/yr/r/fzBXVxs22bH.png"></img>
+      </div>
      
     </div>
   );
