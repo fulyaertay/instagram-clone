bacc4dc84c2ad18c3c778d7c5e931214ff0deb66
Fulya <fulya.ertay@gmail.com>
2023-01-06 23:03:06 +0300

    feat:adjust log in shots

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="h-full w-full flex items-center justify-center ">
-      <div className='w-[380px] h-[581px] bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px]'>
-      
+      <div className='w-[380px] h-[581px] bg-logo-pattern relative bg-[length:468.32px_634.15px] bg-[top_left_-46px]'>
+       <div className='w-[250px] h-[538px] absolute top-[27px] right-[18px]'>
+        <img className="w-full h-full" src="https://www.secure.instagram.com/static/images/homepage/screenshots/screenshot1-2x.png/cfd999368de3.png"></img>
+        </div>
       </div>
      
     </div>
