92e27730bf4eb752e0be22e037a5bbe4a3c23dec
Fulya <fulya.ertay@gmail.com>
2023-01-07 23:53:33 +0300

    added uselocation

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,8 +1,12 @@
-import {Navigate} from 'react-router-dom'
+import {Navigate,useLocation} from 'react-router-dom'
 
 export default function PrivateRoute({children}){
     const user=false;
     if(!user){
+        return <Navigate to="/auth/login" replace={true}
+        state={{
+            return_url:
+        }}></Navigate>
 
     }
     return children
