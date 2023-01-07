e230cca1312a82327ac3cfbad8a10b58382f9bb0
Fulya <fulya.ertay@gmail.com>
2023-01-07 13:22:16 +0300

    added input.js

---
+++ src/components/input.js
@@ -0,0 +1,16 @@
+export default function input({label, ...props}){
+    return(
+        <label className="block relative">
+        <input
+          required={true}
+
+          className="bg-zinc-50 border px-2 outline-none text-xs focus:border-gray-400 rounded-sm w-full valid:pt-[10px] h-[38px] peer" {...props}
+        
+        ></input>
+        <small className="cursor-text pointer-events-none absolute top-1/2 left-[9px] text-xs peer-valid:text-[10px] peer-valid:top-2.5 text-gray-500 transition-all -translate-y-1/2">
+         {label}
+        </small>
+      </label>
+    )
+
+}
