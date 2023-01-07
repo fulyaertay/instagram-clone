c378f789f1991c927342c52e38c8c89e99754608
Fulya <fulya.ertay@gmail.com>
2023-01-07 20:23:38 +0300

    button type changed

--- src/components/input.js
+++ src/components/input.js
@@ -1,16 +1,16 @@
 import { useEffect, useRef, useState } from "react"
 
 export default function Input({label, type='text', ...props}){
-    const inputRef=useRef()
+    
     const [show,setShow]=useState(false)
     const [inputType,setType]=useState(type)
     useEffect(()=>{
         if(show){
             setType('text')
-            inputRef.current.focus()
+
         }else if (type==='password'){
             setType('password')
-            inputRef.current.focus()
+
         }
 
     },[show])
@@ -18,7 +18,7 @@ export default function Input({label, type='text', ...props}){
         <label className="block relative flex bg-zinc-50 rounded-sm border focus-within:border-gray-40" >
         <input
           required={true}
-          ref={inputRef}
+        
           type={inputType}
           className=" px-2 outline-none 0 w-full valid:pt-[10px] h-[38px] peer" {...props}
         
@@ -27,9 +27,9 @@ export default function Input({label, type='text', ...props}){
          {label}
         </small>
         {type==='password' && props?.value && (
-            <button type="button" onClick={()=> setShow(show=>!show)} className="text-sm font-semibold pr-2  h-full flex items-center ">
+            <div type="button" onClick={()=> setShow(show=>!show)} className="cursor-pointer text-sm font-semibold pr-2  h-full flex items-center ">
                 {show? 'Hide' : 'Show'}
-            </button>
+            </div>
           )}
       </label>
     )
