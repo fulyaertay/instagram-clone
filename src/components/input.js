74e56e840291b1faeaeccb57d7ed063090fd2392
Fulya <fulya.ertay@gmail.com>
2023-01-07 13:55:10 +0300

    added show/hide for pswd

--- src/components/input.js
+++ src/components/input.js
@@ -1,19 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-export default function Input({label, ...props}){
+export default function Input({label, type='text', ...props}){
     const [show,setShow]=useState(false)
+    const [inputType,setType]=useState(type)
+    useEffect(()=>{
+        if(show){
+            setType('text')
+        }else if (type==='password'){
+            setType('password')
+        }
+
+    },[show])
     return(
         <label className="block relative">
         <input
           required={true}
-
+          type={inputType}
           className="bg-zinc-50 border px-2 outline-none text-xs focus:border-gray-400 rounded-sm w-full valid:pt-[10px] h-[38px] peer" {...props}
         
         ></input>
         <small className="cursor-text pointer-events-none absolute top-1/2 left-[9px] text-xs peer-valid:text-[10px] peer-valid:top-2.5 text-gray-500 transition-all -translate-y-1/2">
          {label}
         </small>
-        {props?.type==='password' && (
+        {type==='password' && props?.value && (
             <button type="button" onClick={()=> setShow(show=>!show)} className="text-sm font-semibold pr-2 absolute top-0 right-0  h-full flex items-center ">
                 {show? 'Hide' : 'Show'}
             </button>
