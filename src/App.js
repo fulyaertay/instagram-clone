66e3a545e6c2caea583d9c8c45136e3853be243d
Fulya <fulya.ertay@gmail.com>
2023-01-07 15:20:54 +0300

    add sign up link

--- src/App.js
+++ src/App.js
@@ -57,36 +57,61 @@ function App() {
           ></img>
         </div>
       </div>
-
-      <div className="h-[350px] bg-white border px-[40px] pt-10 pb-2">
-        <a href="" className="flex justify-center mb-8">
-          <img
-            className="h-[51px]"
-            src="https://www.secure.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
-          ></img>
-        </a>
-        <form className="grid gap-y-1.5">
-          <Input type="text" value={username}   onChange={e=> setUsername(e.target.value)} label="Phone number,username,email"></Input>
-          <Input type="password" value={password}   onChange={e=> setPassword(e.target.value)} label="Password"></Input>
-    
-          <button type="submit" disabled={!enable} className="h-[30px] rounded bg-brand font-medium text-white text disabled:opacity-50">Log In</button>
-          <div className="flex items-center">
-            <div className="h-px bg-gray-300 flex-1"></div>
-            <span className="px-4 text-[13px] text-gray-500 font-semibold">OR</span>
-            <div className="h-px bg-gray-300 flex-1">
-            </div>
-          </div>
-        
-          <a href="#" className="flex justify-center items-center gap-x-2 text-sm font-semibold text-facebook">
-          <AiFillFacebook size={20}>
-          </AiFillFacebook>
-          Log in with Facebook</a>
-          <a href="#" className="text-xs flex items-center justify-center text-link">
-            Forgot password?
+      <div className="w-[350px] grid gap-y-3">
+        <div className=" bg-white border px-[40px] pt-10 pb-6">
+          <a href="" className="flex justify-center mb-8">
+            <img
+              className="h-[51px]"
+              src="https://www.secure.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+            ></img>
           </a>
-        </form>
+          <form className="grid gap-y-1.5">
+            <Input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              label="Phone number,username,email"
+            ></Input>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              label="Password"
+            ></Input>
 
-      
+            <button
+              type="submit"
+              disabled={!enable}
+              className="h-[30px] mt-1 rounded bg-brand font-medium text-white text disabled:opacity-50"
+            >
+              Log In
+            </button>
+            <div className="flex items-center my-2.5 mb-3.5">
+              <div className="h-px bg-gray-300 flex-1"></div>
+              <span className="px-4 text-[13px] text-gray-500 font-semibold">
+                OR
+              </span>
+              <div className="h-px bg-gray-300 flex-1"></div>
+            </div>
+
+            <a
+              href="#"
+              className="flex justify-center mb-2.5 items-center gap-x-2 text-sm font-semibold text-facebook"
+            >
+              <AiFillFacebook size={20}></AiFillFacebook>
+              Log in with Facebook
+            </a>
+            <a
+              href="#"
+              className="text-xs flex items-center justify-center mb-2 text-link"
+            >
+              Forgot password?
+            </a>
+          </form>
+        </div>
+        <div className="bg-white border p-4 text-sm text-center">
+          Don't have an account? <a href="#" className="font-semibold text-brand">Sign up</a>
+        </div>
       </div>
     </div>
   );
--- src/components/input.js
+++ src/components/input.js
@@ -20,7 +20,7 @@ export default function Input({label, type='text', ...props}){
           required={true}
           ref={inputRef}
           type={inputType}
-          className=" px-2 outline-none text-xs 0 w-full valid:pt-[10px] h-[38px] peer" {...props}
+          className=" px-2 outline-none 0 w-full valid:pt-[10px] h-[38px] peer" {...props}
         
         ></input>
         <small className="cursor-text pointer-events-none absolute top-1/2 left-[9px] text-xs peer-valid:text-[10px] peer-valid:top-2.5 text-gray-500 transition-all -translate-y-1/2">
