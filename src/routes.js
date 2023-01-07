4172e002a090b0dfeb7a482a2b92059ee24759c8
Fulya <fulya.ertay@gmail.com>
2023-01-07 21:00:59 +0300

    feat:login route added

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
+import AuthLayout from './pages/auth';
 import Home from './pages/home';
 import Login from './pages/login'
 
 const routes =[{
     path:'/',
-    element: <Home></Home>
+    element: <Home></Home>,
+    auth:true
 
 }
 ,{
-    path:'/login',
-    element:<Login></Login>
+    path:'/auth',
+    element:<AuthLayout></AuthLayout>,
+    children:[
+        {
+            path:'login',
+            element:<Login></Login>
+        }
+    ]
 }
 
 ]
