d1e618b9981aace447f8715f6653cfcce2b89fb4
Fulya <fulya.ertay@gmail.com>
2023-01-07 23:58:22 +0300

    feat: auto routing added

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,10 +2,11 @@ import {Navigate,useLocation} from 'react-router-dom'
 
 export default function PrivateRoute({children}){
     const user=false;
+    const location=useLocation()
     if(!user){
         return <Navigate to="/auth/login" replace={true}
         state={{
-            return_url:
+            return_url:location.pathname
         }}></Navigate>
 
     }
--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,33 @@
-import AuthLayout from './pages/auth';
-import Home from './pages/home';
-import Login from './pages/login'
-
-const routes =[{
-    path:'/',
+import AuthLayout from "./pages/auth";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import PrivateRoute from "./components/PrivateRoute";
+const routes = [
+  {
+    path: "/",
     element: <Home></Home>,
-    auth:true
-
-}
-,{
-    path:'/auth',
-    element:<AuthLayout></AuthLayout>,
-    children:[
-        {
-            path:'login',
-            element:<Login></Login>
-        }
-    ]
-}
-
-]
-
-export default routes;
+    auth: true,
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout></AuthLayout>,
+    children: [
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+];
+const authCheck = (routes) => {
+  routes.map((route) => {
+    if (route?.auth) {
+      route.element = <PrivateRoute>{route.element}</PrivateRoute>;
+    }
+    if (route?.children) {
+      route.children = authCheck(route.children);
+    }
+    return route;
+  });
+};
+export default authCheck(routes);
