0a829fa635341761f590b9fd664c3644690c5b68
Fulya <fulya.ertay@gmail.com>
2023-01-08 10:31:26 +0300

    created store for login

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
